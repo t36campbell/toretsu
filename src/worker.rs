69d8804be25d43c55d046ca908a0d6936f72213f
t36campbell <campbell3636@yahoo.com>
2025-07-20 23:35:36 -0400

    updated worker

--- src/worker.rs
+++ src/worker.rs
@@ -1,14 +1,24 @@
 use names::{Generator, Name};
+use std::sync::{Arc, Mutex};
+use std::thread;
+use std::time::Duration;
 use uuid::Uuid;
 
 use crate::queue::Queue;
 use crate::task::Task;
 
+enum WorkerState<T> {
+    Idle(Queue<T>),
+    Running {
+        shared_queue: Arc<Mutex<Queue<T>>>,
+        shared_active: Arc<Mutex<bool>>,
+    },
+}
+
 pub struct Worker<T> {
     pub id: Uuid,
     pub channel: String,
-    pub queue: Queue<T>,
-    pub active: bool,
+    state: WorkerState<T>,
 }
 
 impl<T> Default for Worker<T>
@@ -50,9 +60,8 @@ where
 
         Self {
             id,
-            queue,
             channel,
-            active: true,
+            state: WorkerState::Idle(queue),
         }
     }
 
@@ -62,27 +71,134 @@ where
 
     pub fn from(vec: Vec<T>) -> Self {
         let queue = Queue::from(vec);
-
         Self::init(None, None, Some(queue))
     }
 
     pub fn clock_in(&mut self) {
-        self.active = true;
-
-        for mut item in self.queue.drain_sorted() {
-            rayon::spawn(move || item.process());
+        if matches!(self.state, WorkerState::Running { .. }) {
+            return;
         }
+
+        let queue = match std::mem::replace(&mut self.state, WorkerState::Idle(Queue::new())) {
+            WorkerState::Idle(queue) => queue,
+            WorkerState::Running { .. } => unreachable!(),
+        };
+
+        let shared_queue = Arc::new(Mutex::new(queue));
+        let shared_active = Arc::new(Mutex::new(true));
+
+        self.state = WorkerState::Running {
+            shared_queue: Arc::clone(&shared_queue),
+            shared_active: Arc::clone(&shared_active),
+        };
+
+        thread::spawn(move || loop {
+            {
+                let is_active = shared_active.lock().unwrap();
+                if !*is_active {
+                    break;
+                }
+            };
+
+            let task = {
+                let mut guard = shared_queue.lock().unwrap();
+                guard.pop()
+            };
+
+            match task {
+                Some(mut t) => {
+                    rayon::spawn(move || t.process());
+                }
+                None => {
+                    thread::sleep(Duration::from_millis(10));
+                }
+            }
+        });
     }
 
     pub fn clock_out(&mut self) {
-        self.active = false
+        let (shared_queue, shared_active) = match &self.state {
+            WorkerState::Idle(_) => return,
+            WorkerState::Running {
+                shared_queue,
+                shared_active,
+            } => (Arc::clone(shared_queue), Arc::clone(shared_active)),
+        };
+
+        {
+            let mut active = shared_active.lock().unwrap();
+            *active = false;
+        }
+
+        thread::sleep(Duration::from_millis(50));
+        let recovered_queue = match Arc::try_unwrap(shared_queue) {
+            Ok(mutex) => mutex.into_inner().unwrap(),
+            Err(shared_queue) => {
+                if let Ok(mut queue_guard) = shared_queue.lock() {
+                    let remaining_tasks: Vec<_> = queue_guard.drain().collect();
+                    Queue::from(remaining_tasks)
+                } else {
+                    Queue::new()
+                }
+            }
+        };
+
+        self.state = WorkerState::Idle(recovered_queue);
+    }
+
+    pub fn is_running(&self) -> bool {
+        matches!(self.state, WorkerState::Running { .. })
+    }
+
+    pub fn queue_len(&self) -> usize {
+        match &self.state {
+            WorkerState::Idle(queue) => queue.len(),
+            WorkerState::Running { shared_queue, .. } => {
+                let queue = shared_queue.lock().unwrap();
+                queue.len()
+            }
+        }
+    }
+
+    pub fn queue_is_empty(&self) -> bool {
+        match &self.state {
+            WorkerState::Idle(queue) => queue.is_empty(),
+            WorkerState::Running { shared_queue, .. } => {
+                let queue = shared_queue.lock().unwrap();
+                queue.is_empty()
+            }
+        }
+    }
+
+    pub fn is_active(&self) -> bool {
+        match &self.state {
+            WorkerState::Idle(_) => false,
+            WorkerState::Running { shared_active, .. } => {
+                let active = shared_active.lock().unwrap();
+                *active
+            }
+        }
     }
 
     pub fn assign_one(&mut self, task: T) {
-        self.queue.push(task);
+        match &mut self.state {
+            WorkerState::Idle(queue) => queue.push(task),
+            WorkerState::Running { shared_queue, .. } => {
+                let mut queue = shared_queue.lock().unwrap();
+                queue.push(task);
+            }
+        }
     }
 
-    pub fn assign_many<I: IntoIterator<Item = T>>(&mut self, iter: I) {
-        self.queue.extend(iter);
+    pub fn assign_many(&mut self, vec: Vec<T>) {
+        match &mut self.state {
+            WorkerState::Idle(queue) => {
+                queue.extend(vec);
+            }
+            WorkerState::Running { shared_queue, .. } => {
+                let mut queue = shared_queue.lock().unwrap();
+                queue.extend(vec);
+            }
+        }
     }
 }
